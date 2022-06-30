b76a2b9ecd78cb2048f00b66df41b0798d81facd
Andrei Rozhkovskii <andrei.roz@fora-soft.com>
2022-06-30 16:23:21 +0300

    Initial Commit

---
+++ InStatTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InStatTest
+//
+//  Created by andy on 30.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
